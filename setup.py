036eb0ceb8bfddf89fdd26a75a4f98f5109eef4d
Irving Leonard <irving@.(none)>
2012-05-16 16:15:36 -0400

    completed setup

--- setup.py
+++ setup.py
@@ -3,38 +3,32 @@ from distutils.core import setup
 with open('README') as file:
     long_description = file.read()
 
-# There's still some work to do;
-# - Check the classifiers
-# - Get the GitHub repo URL
-# - Make a release in github and use the URL
-# - Check platforms parameter
-# - Check license parameter
+# There's still some work to do:
 # - Work in the platform specific files handling (the cron job)
 	
 setup(name='pylogan',
 	version = '0.3.2',
 	author = 'Irving Leonard',
 	author_email = 'irvingleonard@ymail.com',
-	url = 'TODO',
+	url = 'https://github.com/irvingleonard/pylogan',
 	description = 'Modular Log Analayzer',
 	long_description = long_description,
-	download_url = 'TODO',
+	download_url = 'https://github.com/downloads/irvingleonard/pylogan/pylogan-0.3.2.zip',
 	classifiers=[
-		'Development Status :: 2 - Pre-Alpha',
+		'Development Status :: 3 - Alpha',
 		'Environment :: Console',
 		'Intended Audience :: Developers',
 		'Intended Audience :: System Administrators',
 		'License :: OSI Approved :: BSD License',
-		'Operating System :: ANY',
-		'Programming Language :: Python',
+		'Natural Language :: Spanish',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
 		'Topic :: Communications',
 		'Topic :: Internet :: Log Analysis',
 		'Topic :: System :: Logging',
 		'Topic :: System :: Monitoring',
 		'Topic :: System :: Systems Administration',
 		],
-	platforms=['ANY'],
-	license='BSD',
 	provides=['pylogan'],
 	requires=['sys','os','os.path','datetime','re','socket','collections','xml.dom.minidom',],
 	package_dir = {'pylogan': 'bin/pylogan'},
